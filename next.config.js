d3c053254e3411d6ffbdb9c8779e5aefe21c5c29
IKKU <iqbalsuperfuture@gmail.com>
2024-02-16 15:38:09 +0400

    basepath

--- next.config.js
+++ next.config.js
@@ -107,8 +107,6 @@ module.exports = withBundleAnalyzer({
     scrollRestoration: true
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true
   },
   exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
