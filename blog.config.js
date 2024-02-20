e553faa5bfe6807e8015606f61ae1cf6ed79b2a5
IKKU <iqbalsuperfuture@gmail.com>
2024-02-20 11:44:41 +0400

    fix

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Joyboy', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'Joyboy', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://joyboy.ae', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://joyboy.ae/blog', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Joyboy.ae', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
